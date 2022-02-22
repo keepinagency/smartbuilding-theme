7d37ce3d3d2069663423af0561630412a85728ab
Jhon <jhonwalter.b2002@gmail.com>
2022-02-22 17:48:56 -0400

    footer

--- footer.php
+++ footer.php
@@ -15,81 +15,75 @@
 ?>
 </div--><!-- /.row -->
 </div><!-- /.container -->
-<footer id="contacto" class="footer">
-	<div class="col-12 d-lg-flex flex-lg-row-reverse pl-lg-5">
-		<!--div class="col-12 col-lg-6 pb-lg-4">
-				<!--TEXTO Y BOTON-->
-			<!--div class="col-12 col-lg-10 pl-3 pt-4 texto-footer">
-				<?php echo $informa;?>
+<!-- Footer -->
+ <!-- Grid container -->
+<footer class="bg-light text-center ">
+	<div class="container p-4">
+		<div class="row">
+			<div class="col">
+				<!-- Section: Social media -->
+				<section class="mb-4">
+					<!-- RSS -->
+					<p>
+					Nuestras Redes
+					</p>
+					<a href="" class="me-4 text-reset text-decoration-none">
+						<img src="<?= smartbuilding_IMG. 'rss.jpeg'?>"/>
+					</a>
+
+					<!-- Facebook -->
+					<a href="" class="me-4 text-reset text-decoration-none">
+						<img src="<?= smartbuilding_IMG. 'facebook.jpeg'?>"/>
+					</a>
+
+					<!-- Gplus -->
+					<a href="" class="me-4 text-reset text-decoration-none">
+						<img src="<?= smartbuilding_IMG. 'gplus.jpeg'?>"/>
+					</a>
+
+					<!-- LinkedIn -->
+					<a href="" class="me-4 text-reset text-decoration-none">
+						<img src="<?= smartbuilding_IMG. 'linked-in.jpeg'?>"/>
+					</a>
+
+					<!-- Pinterest -->
+					<a href="" class="me-4 text-reset text-decoration-none">
+					<img src="<?= smartbuilding_IMG. 'pinterest.jpeg'?>"/>
+					</a>
+					</section>
+					<!-- Section: Social media -->
 			</div>
-			<div class="col-12 col-lg-10 ">
-				<form class="form-inline"
-						method="post"
-						action="<?php echo get_template_directory_uri().'/temp_parts/solicitamasinfo.php'; ?>" validate>
-					<div class="form-group col-10 p-0 m-0">
-						<input type="email" id="correo" name="correo"
-								class="form-control-plaintext bg-white texto-btn pl-3 pt-2 " 
-								placeholder="<?php echo $placeh;?>" required>
-					</div>
+			<div class="col">
+				<p>
+					Contacto
+				</p>
+				<p>
+					<a href="" class="me-4 text-reset text-decoration-none">
+					</a>El Juncal #901 Bodega 3 Quilicura - Santiago
+				</p>
+				<p>
+					<a href="" class="me-4 text-reset text-decoration-none">
+					</a>info@example.com
+				</p>
+				<p>
+					<a href="" class="me-4 text-reset text-decoration-none">
+					</a> +56 22 9380016
 
-					<div class="form-group col-2 p-0 m-0 ">
-						<button type="submit" class="btn p-0 m-0">
-							<img src="<?php echo get_template_directory_uri() . '/img/btn-vermas_trans_solo.png'; ?> " 
-							class="img-btn-foot">
-						</button>
-					</div>
-
-				</form>
+				</p>		
 			</div>
-		</div-->
-
-		<!--SOCIAL MEDIA-->
-		<!--div class="col-12 p-0 pt-3 pb-3 d-flex flex-row align-items-center
-					col-lg-6 pl-lg-4 justify-content-lg-center">
-
-			<a href="<?php echo $url_int; ?>" target="_blank" class="pl-4">
-				<img src="<?php echo $instalogo; ?>" alt="" id="">
-			</a>
-			<a href="<?php echo $url_fbk; ?>" target="_blank" class="pl-4">
-				<img src="<?php echo $facelogo;?>" alt="" id="">
-			</a>
-			<a href="<?php echo $url_lik;?> " target="_blank" class="pl-4">
-				<img src="<?php echo $linkelogo;?>" alt="" id="">
-			</a>
-			<a href="<?php echo $url_git;?>" target="_blank" class="pl-4">
-				<img src="<?php echo $gitlogo;?>" alt="" id="">
-			</a>
-
-		</div-->
+		</div>
 	</div>
-
-	<!--MENÚ-->
-	<!--div class="col-12 d-md-flex flex-md-row p-0 justify-content-center">
-		<nav class="navbar navbar-ligth col-12 p-0 m-0">
-			<div class="col-12 p-0 pb-lg-0 pt-lg-0 h-100">
-				<?php
-					wp_nav_menu( array(
-						'container'       => 'div',
-						'container_class' => 'col-12 col-lg-12 d-lg-block p-0 m-0',
-						'container_id'    => 'idFooterMenu',
-						'items_wrap'      => '<ul id="%1$s" class="%2$s p-0 m-0 w-100 nav nav-tabs justify-content-center ">%3$s</ul>',
-						'theme_location'  => 'footer-menu',
-						'menu_class'      => 'footer-menu',
-						'walker'          => new WP_Bootstrap_Navwalker())
-					);
-				?>
-			</div>
-		</nav>
-	</div-->
-	<!--DIR URL-->
-	<div class="bg-white col-12 text-center pt-1 pb-3 p-lg-2">
-		<p>Otro sitio web desarrollado, a medida, por KeepIn Agency. <a class="dirurl-footer" href="https://www.keepinagency.com">www.keepinegancy.com</a> </p>
-		<!--p>Copyright © 2016 SmartBuilding. Diseñado y Desarrollado por <a class="dirurl-footer" href="https://www.keepinagency.com">www.keepinegancy.com</a> </p-->
-	</div>
-	
 </footer>
 
 
+  <!-- Copyright -->
+  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+    © 2022 Copyright:
+    <a class="text-reset fw-bold" href="https://mdbootstrap.com/">Keepin Agency</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
 
 <?php wp_footer(); ?>
 </body>
