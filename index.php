92a8bed12879005a6282221f0028ed24a9b8f9b2
m3stiz0 <kmestizo@gmail.com>
2022-06-21 17:30:42 -0400

    Se creo plantilla HomePage, se comento todo el index

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php 
     get_header(); 
-        require_once(smartbuilding_TEMP_PARTS . '/home-inicio.php');
+        /*require_once(smartbuilding_TEMP_PARTS . '/home-inicio.php');
         require_once(smartbuilding_TEMP_PARTS . '/home-productos.php');
         require_once(smartbuilding_TEMP_PARTS . '/home-servicios.php');
         //require_once(smartbuilding_TEMP_PARTS . '/home-testimonios.php');
-        require_once(smartbuilding_TEMP_PARTS . '/home-contacto.php');
+        require_once(smartbuilding_TEMP_PARTS . '/home-contacto.php');*/
     get_footer();
 ?>
